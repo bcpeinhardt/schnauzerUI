fc7f6ee5a5fd05b8d06db26bc1b7eb20448ca657
Benjamin Peinhardt <benjaminpeinhardt@gmail.com>
2022-12-13 14:38:45 -0600

    Added comments to environment.

--- src/environment.rs
+++ src/environment.rs
@@ -1,5 +1,7 @@
 use std::collections::HashMap;
 
+/// Represents the "state" of the programs execution. Basically
+/// keeps charge of variables and their values.
 pub struct Environment(HashMap<String, String>);
 
 impl Environment {
@@ -7,10 +9,14 @@ impl Environment {
         Self(HashMap::new())
     }
 
+    /// Set a variable value. SchnauzerUI makes no distinction between
+    /// declaration and instantiation.
     pub fn set_variable(&mut self, name: String, value: String) {
         self.0.insert(name, value);
     }
 
+    /// Get the value of a variable if it exists, or None
+    /// if it does not.
     pub fn get_variable(&self, name: &str) -> Option<String> {
         self.0.get(name).map(|s| s.clone())
     }
