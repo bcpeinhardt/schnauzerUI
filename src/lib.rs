83c60a326777443b432a0cfe81212f7da8d3eb8e
Benjamin Peinhardt <benjaminpeinhardt@gmail.com>
2022-12-19 00:56:46 -0600

    ugh

--- src/lib.rs
+++ src/lib.rs
@@ -6,7 +6,6 @@
 //! a QA engineer (see [Cucumber](https://cucumber.io/)), SchnauzerUI is the only source of truth for a
 //! test's execution. In this way, SchnauzerUI aims to provide a test report you can trust.
 //! 
-//!
 //! SchnauzerUI is most comparable to and could serve as an open source replacement for [testRigor](https://testrigor.com/)
 //!
 //! If you would like to try it out, you can start with the [narrative docs](https://bcpeinhardt.github.io/schnauzerUI/)
